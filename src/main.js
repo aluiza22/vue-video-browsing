0e5e3bcab5cd6dcb2fc85c42ef715ca5381d1da8
Ana Luiza <aluiza.silva22@gmail.com>
2018-05-19 16:32:43 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+	
+	render: h => h(App)
+	/*el: '#app',
+	render: function(createElement) {
+		return createElement(App);
+	}*/
+}).$mount('#app');
