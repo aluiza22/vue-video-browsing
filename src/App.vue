ab512e70a03b6c6315fa1f8416c09b8ce939f115
Ana Luiza <aluiza.silva22@gmail.com>
2018-05-20 15:14:36 -0300

    working application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-	<div>
-		<SearchBar></SearchBar>
+	<div class="container">
+		<SearchBar @termChange="onTermChange" ></SearchBar>
+		<div class="row">
+			<VideoDetail :video="selectedVideo"></VideoDetail>
+			<VideoList @videoSelect="onVideoSelect" :videos="videos" ></VideoList>
+		</div>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios';
 	import SearchBar from './components/SearchBar';
+	import VideoList from './components/VideoList';
+	import VideoDetail from './components/VideoDetail';
+	const API_KEY = 'AIzaSyD7tD-QRyyJ_mevC2ATaS6Jo8VPcmnnZtA';
 
 	export default {
 		name: 'App',
 		components: {
-			SearchBar
+			SearchBar,
+			VideoList,
+			VideoDetail
+		},
+		data: function() {
+			return { videos: [], selectedVideo: {} };
+		},
+		methods: {
+			onVideoSelect: function(video) {
+				this.selectedVideo = video;
+			},
+			onTermChange: function(searchTerm) {
+				axios.get('https://www.googleapis.com/youtube/v3/search', {
+					params: {
+						key: API_KEY,
+						type: 'video',
+						part: 'snippet',
+						q: searchTerm
+					}
+				})
+				.then(response => {
+					this.videos = response.data.items;
+				});
+			}
 		}
 	};
 </script>
